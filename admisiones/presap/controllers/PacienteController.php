707eadffabb5a321be1bcf9a12fc453058f5a479
Edier Andrés Villaneda Navarro <edvillaneda@personalsoft.com.co>
2021-04-03 12:02:40 -0500

    implementacion de generacion de stickers en pdf

---
+++ admisiones/presap/controllers/PacienteController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Admisiones\controllers;
+
+require dirname(__FILE__) . '/../models/Paciente.php';
+
+use Admisiones\Models\Paciente;
+use Exception;
+
+/*
+ * Datos socioeconomicos de un paciente.
+ *
+ * @author Edier Andres Villaneda Navarro
+ * @version 1.0
+ */
+
+class PacienteController extends Paciente
+{
+
+    private $errorCode;
+    public $request;
+    private $response;
+
+    /**
+     * Consultar los datos socioeconomicos de un paciente.
+     * @param object $conex          Datos de la conexión a la base de datos. (Obligatorio)
+     * @param string $empresa        Alias de la empresa
+     * @param        $numeroHistoria Número de Historia Clinica del paciente.
+     * @param        $numeroIngreso  Numero de ingreso del paciente.
+     */
+    public function __construct($conex, string $empresa, $numeroHistoria, $numeroIngreso)
+    {
+        $this->setNumeroHistoria($numeroHistoria);
+        $this->setNumeroIngreso($numeroIngreso);
+        $this->error = "";
+        if ($this->validarDatos()) {
+            $this->getDatosPaciente($conex, $empresa);
+        }
+    }
+
+    /**
+     * Validar si los numeros de Historia e Ingreso son validos.
+     * @return boolean
+     * @throws Exception
+     */
+    public function validarDatos(): bool
+    {
+        $this->errorCode = false;
+        switch (true) {
+            case $this->getNumeroHistoria() == 0:
+                $this->errorCode = true;
+                throw new \Exception("Número de Historia invalido");
+                break;
+            case $this->getNumeroIngreso() == 0:
+                $this->errorCode = true;
+                throw new \Exception("Número de Ingreso invalido");
+                break;
+        }
+        if ($this->errorCode) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Serializacion de datos de la consulta
+     * @return array array de datos del paciente
+     */
+    function jsonSerialize(): array
+    {
+        return [
+            'numeroHistoria' => $this->getNumeroHistoria(),
+            'numeroIngreso' => $this->getNumeroIngreso(),
+            'documento' => $this->getDocumento(),
+            'tipoDocumento' => $this->getTipoDocumento(),
+            'numeroDocumento' => $this->getNumeroDocumento(),
+            'nombreCompleto' => $this->getNombreCompleto(),
+            'nombres' => $this->getNombres(),
+            'apellidos' => $this->getApellidos(),
+            'aseguradora' => $this->getAseguradora(),
+            'diagnostico' => $this->getDiagnostico(),
+            'edad' => $this->getEdad()
+        ];
+
+    }
+}
--- admisiones/reportes/stickers/stickers.php
+++ admisiones/reportes/stickers/stickers.php
@@ -1,45 +1,39 @@
 <?php
-    include_once("conex.php");
+include_once("conex.php");
 
-    include dirname(__FILE__) . '/../../presap/controllers/DatosSocioEconomicosController.php';
-    include dirname(__FILE__) . '/../../presap/controllers/StickerPDFController.php';
+include dirname(__FILE__) . '/../../presap/controllers/PacienteController.php';
+include dirname(__FILE__) . '/../../presap/service/StickerPDFService.php';
 
-    use Admisiones\Controller\DatosSocioEconomicosController as DatosPaciente;
-    use Admisiones\Controller\StickerPDFController;
+use Admisiones\controllers\PacienteController;
+use Admisiones\service\StickerPDFService;
 
-//    if (isset($_GET['wemp_pmla'])) {
-//        $wEmp = $_GET['wemp_pmla'];
-//    }
-    $wEmp = empty($_GET['wemp_pmla']) ? 0 : $_GET['wemp_pmla'];
-    $nHis = empty($_GET['nHis']) ? 0 : $_GET['nHis'];
-    $nIng = empty($_GET['nIng']) ? 0 : $_GET['nIng'];
-    $empresa = empty($_GET['empresa']) ? null : $_GET['empresa'];
-    $json = empty($_GET['json']) ? false : $_GET['json'];
+$wEmp = empty($_GET['wemp_pmla']) ? 0 : $_GET['wemp_pmla'];
+$nHis = empty($_GET['nHis']) ? 0 : $_GET['nHis'];
+$nIng = empty($_GET['nIng']) ? 0 : $_GET['nIng'];
+$empresa = empty($_GET['empresa']) ? null : $_GET['empresa'];
+$json = empty($_GET['json']) ? false : $_GET['json'];
 
-//    echo "$wEmp $nHis $nIng";
-
-//$conex = obtenerConexionBD("matrix");
-
-    $datosPaciente = new DatosPaciente($conex, $empresa, $nHis, $nIng);
-    if($json){
+try {
+    $paciente = new PacienteController($conex, $empresa, $nHis, $nIng);
+    if ($json) {
         header('Content-Type: text/json');
-
-        echo json_encode($datosPaciente->jsonSerialize());
+        echo json_encode($paciente->jsonSerialize());
     } else {
-        try {
-            $pdf = new StickerPDFController('L', 'mm', [50, 38]);
-            $pdf->generarSticker($empresa,
-                $datosPaciente->getNombreCompleto(),
-                $datosPaciente->getDocumento(),
-                $datosPaciente->getNumeroHistoria(),
-                $datosPaciente->getNumeroIngreso(),
-                $datosPaciente->getGenero(),
-                $datosPaciente->getEdad(),
-                $datosPaciente->getDiagnostico(),
-                $datosPaciente->getAseguradora());
-            $pdf->Output();
-        } catch (Exception $e) {
-            die("Error intentando generar el sticker");
-        }
-
+        $pdf = new StickerPDFService('L', 'mm', [50, 38]);
+        $pdf->generarSticker(
+            $paciente->getNombreCompleto(),
+            $paciente->getDocumento(),
+            $paciente->getNumeroHistoria(),
+            $paciente->getNumeroIngreso(),
+            $paciente->getGenero(),
+            $paciente->getEdad(),
+            $paciente->getDiagnostico(),
+            $paciente->getAseguradora(),
+            $empresa
+        );
+        $pdf->Output();
     }
+} catch (Exception $e) {
+    die("Error intentando generar el sticker");
+}
+
