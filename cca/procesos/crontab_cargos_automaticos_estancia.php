cb2a866ea74f26a1f9a045ed72732842fdaa2581
edwin.molina1 <emolinagrisales@gmail.com>
2021-05-05 15:26:20 -0500

    Se mejora querie que trae los pacientes para liquidar la estancia automática

--- cca/procesos/crontab_cargos_automaticos_estancia.php
+++ cca/procesos/crontab_cargos_automaticos_estancia.php
@@ -9,30 +9,30 @@ if( isset($_GET['wemp_pmla']) && $wemp_pmla != '' ) {
 
 	$wbasedato_movhos = consultarAliasPorAplicacion($conex,$wemp_pmla,'movhos');
 	$wbasedato_cliame = consultarAliasPorAplicacion($conex, $wemp_pmla, 'facturacion');
-		
+	
 	$sql = "SELECT Ubihis,Ubiing 
 			  FROM ".$wbasedato_movhos."_000018 
 			  JOIN ".$wbasedato_cliame."_000100 
 				ON Ubihis = Pachis 
 			  JOIN ".$wbasedato_movhos."_000011  
 				ON Ccocod = Ubisac 
+			  JOIN ".$wbasedato_movhos."_000020  
+				ON Habhis = Ubihis
+				AND Habing = Ubiing
 			 WHERE Ubiald = 'off' 
 			   AND Ubialp = 'off' 
 			   AND ccohos = 'on'";
-
+	
 	$res = mysql_query($sql,$conex) or die("Error: " . mysql_errno() . " - en el query (Consultar Pacientes Estancia): ".$sql." - ".mysql_error());
 	$arr = array();
 	
 	while($row = mysql_fetch_array($res))
 	{
-		
 		$whis = $row['Ubihis'];
 		$wing = $row['Ubiing'];
 		
 		guardarCargoAutomaticoEstancia($conex, $wemp_pmla, $wbasedato_movhos, $wbasedato_cliame, $whis, $wing);
-		
 	}
-
 }
 
 ?>
