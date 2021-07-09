6294d0e77232059ddb47f311a0392cddf3b77dfc
Edwin Molilna Grisales <edwin.molina@pmla.local>
2021-07-09 11:34:02 -0500

    Agregando log para autorizaciones

--- hce/procesos/ordenes.inc.php
+++ hce/procesos/ordenes.inc.php
@@ -38703,8 +38703,8 @@ function consultarProtocolo($wbasedato,$protocolo,$cco,$codUsuario){
 			if($rs['Procco']=="*" || $rs['Procco']==$cco)	
 			{
 				
-				if (!array_key_exists($rs['Dprcod']."-".$rs['Dprpes'], $arrayDetalleProtocolo))
-				{
+				// if (!array_key_exists($rs['Dprcod']."-".$rs['Dprpes'], $arrayDetalleProtocolo))
+				// {
 					$consulta .= 
 					$rs['Dprcod'].
 					"|".$rs['Dprfre'].
@@ -38720,7 +38720,7 @@ function consultarProtocolo($wbasedato,$protocolo,$cco,$codUsuario){
 					"\\";
 					
 					$arrayDetalleProtocolo[$rs['Dprcod']."-".$rs['Dprpes']] = $rs['Dprcod'];
-				}
+				// }
 				
 			}
 			
