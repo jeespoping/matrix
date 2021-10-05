84847b51256212ef30d2cdd44c40b96f59e9e74c
Joel David Payares Hernández <joel.payares@lasamericas.com.co>
2021-10-05 18:04:09 -0500

    Cambio valor quemado departamento

--- hce/procesos/CTCmipres.php
+++ hce/procesos/CTCmipres.php
@@ -68,6 +68,7 @@ include_once("conex.php");
 //--------------------------------------------------------------------------------------------------------------------------------------------                                                                                                                       \\
 			$wactualiz='2020-03-02';
 //--------------------------------------------------------------------------------------------------------------------------------------------                                                                                                                       \\
+//  2021-10-01  Joel Payares Hdz		- Se crea parametro departamento en base de datos para obtener el valor dinamicamente.
 // 	2020-11-13	Edwin MG				- Se cambia función mysqli_connect, que se conectaba a la BD de producción y se cambia por función nueva
 //										  auna_connectdb agregada en el conex.php
 // 	2020-03-02	Jessica Madrid Mejía	- Se agrega el parámetro default_socket_timeout con el tiempo configurado en minutos en 
@@ -4847,6 +4848,7 @@ else
 		// }
 		
 		$municipio   = consultarAliasPorAplicacion( $conex, $wemp_pmla, 'municipioMipres' );
+		$departamento   = consultarAliasPorAplicacion( $conex, $wemp_pmla, 'departamentoMipres' );
 		$nombrePrestador   = consultarAliasPorAplicacion( $conex, $wemp_pmla, 'nombrePrestadorMipres' );
 		
 		$CodVer   = consultarAliasPorAplicacion( $conex, $wemp_pmla, 'codigoVersionMipres' );
