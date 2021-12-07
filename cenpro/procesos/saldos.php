fcaa8a0b0060e7c1e9a5e85a4459304427491014
Sebastián Nevado <snevadov@gmail.com>
2021-12-06 19:35:41 -0500

    Se agrega manejo de log en la función que genera los saldos a fin de mes.

--- cenpro/procesos/saldos.php
+++ cenpro/procesos/saldos.php
@@ -27,6 +27,7 @@
 /******************************************************************************************************************************************
  * Modificaciones:
  *
+ * 2021-12-02:	Se agrega log a base de datos para su ejecución para poder realizar trazabilidad de cuándo se ejecutó y si hubo error.
  * 2020-03-20:	Se hacen modifaciones varias para registrar en tabla saldos de inventarios(cenpro_000014), el insumo y el estado con 
  *				que se encuentra la presentación
  ******************************************************************************************************************************************/
@@ -106,6 +107,34 @@ function bi($d,$n,$k,$i)
 	return -1;
 }
 
+/**
+ * Se inserta la información de la ejecución del cron en la tabla de logs
+ * 
+ * @author	Sebastián Nevado
+ * @since	2021-12-01
+ */
+function insertarLogCron($sFuncion, $sDescripcion)
+{
+	global $conex;
+
+	//Consulta para guartar en la tabla de log
+	$sQuery = "INSERT INTO root_000118 (Medico, Fecha_data, Hora_data, 
+	Logfun, Logdes, Logfue, Loghue, 
+	Logema, Logest, Seguridad)
+	VALUES ('root', ?, ?, 
+	?, ?, ?, ?,
+	'on', 'on', 'C-root')";
+
+	//Preparo y envío los parámetros
+	$sentencia = mysqli_prepare($conex, $sQuery);
+	mysqli_stmt_bind_param($sentencia, "ssssss", date("Y-m-d"), date("H:i:s"), $sFuncion, $sDescripcion, date("Y-m-d"), date("H:i:s"));
+
+	//Guardo en BD
+	$bResultado = (mysqli_stmt_execute($sentencia)) ? true : false;
+
+	return $bResultado;
+}
+
 ///////////////////////////////////////////////////////PROGRAMA/////////////////////////////////////////
 
 	echo "<form name='saldos' action='Saldos.php' method=post>";
@@ -125,98 +154,137 @@ function bi($d,$n,$k,$i)
 		$wano=date('Y');
 	}
 	
-	
-	if(!isset($wano) or !isset($wmes))
-	{
-		$wlinant="";
-		echo "<input type='HIDDEN' name= 'wlinant' value='".$wlinant."'>";
-		echo "<center><table border=0>";
-		echo "<tr><td class='texto4'>Año de Proceso</td>";
-		echo "<td class='texto4'><input type='TEXT' name='wano' value='".date('Y')."' size=4 maxlength=4 readonly=readonly></td></tr>";
-		echo "<tr><td class='texto4'>Mes de Proceso</td>";
-		echo "<td class='texto4'><input type='TEXT' name='wmes' value='".date('m')."' size=2 maxlength=2 readonly=readonly></td></tr>";
-		echo "<tr><td class='texto4' colspan=2 ><input type='submit' value='ENTER'></td></tr></table>";
-	}
-	else
-	{
-		$query = "delete  from ".$empresa."_000014 ";
-		$query .= "  where Salano = ".$wano;
-		$query .= "       and Salmes = ".$wmes;
-		$err = mysql_query($query,$conex) or die(mysql_errno().":".mysql_error());
-
-		echo "<table border=0 align=center>";
-		echo "<tr><td class='texto4'><font face='tahoma'><b>AÑO DE PROCESO : </b>".$wano."</td></tr>";
-		echo "<tr><td class='texto4'><font face='tahoma'><b>MES DE PROCESO : </b>".$wmes."</td></tr></table><br><br>";
-		$ins=array();
-		//consultamos en la tabla de saldos de insumos, las cantidades
-		$query = "SELECT  Appcco, Apppre, Appexi, Appcnv, Appcos, Appcod, Appest  from ".$empresa."_000009 order by Apppre ";
-		$err = mysql_query($query,$conex);
-		$num = mysql_num_rows($err);
-
-		for ($i=0;$i<$num;$i++)
+	$sFuncion = "Cron Saldos - Inicio ".date("Y-m-d")." ".(string)date("H:i:s");
+	$sDescripcion = 'Inicia la ejecución del cron de Saldos '.date("Y-m-d")." ".(string)date("H:i:s");
+	insertarLogCron($sFuncion, $sDescripcion);
+	/**
+     * Se agrega la captura para excepciones en mysqli y el try catch
+     * @by: sebastian.nevado
+     * @date: 2021/12/02
+     */
+	$driver = new mysqli_driver();
+    $driver->report_mode = MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ERROR;
+	try {
+		if(!isset($wano) or !isset($wmes))
 		{
-			$row = mysql_fetch_array($err);
-			$ins[$i]['cco']=$row[0];
-			$ins[$i]['cod']=$row[1];
-			$ins[$i]['exi']=$row[2];
-			$ins[$i]['cnv']=$row[3];
-			$ins[$i]['cos']=$row[4];
-			$ins[$i]['ins']=$row[5];
-			$ins[$i]['est']=$row[6];
+			$wlinant="";
+			echo "<input type='HIDDEN' name= 'wlinant' value='".$wlinant."'>";
+			echo "<center><table border=0>";
+			echo "<tr><td class='texto4'>Año de Proceso</td>";
+			echo "<td class='texto4'><input type='TEXT' name='wano' value='".date('Y')."' size=4 maxlength=4 readonly=readonly></td></tr>";
+			echo "<tr><td class='texto4'>Mes de Proceso</td>";
+			echo "<td class='texto4'><input type='TEXT' name='wmes' value='".date('m')."' size=2 maxlength=2 readonly=readonly></td></tr>";
+			echo "<tr><td class='texto4' colspan=2 ><input type='submit' value='ENTER'></td></tr></table>";
 		}
-
-		//consultamos en la tabla de lotes, los productos con saldo
-		$query = "SELECT plocod, plopro, plosal from ".$empresa."_000004 where plosal>0 and ploest='on' ";
-		$err = mysql_query($query,$conex);
-		$num = mysql_num_rows($err);
-		if ($num>0)
+		else
 		{
-			//consultamos el movimiento de fabricación del lotes
-			$query = "SELECT concod from   ".$empresa."_000008 ";
-			$query .= " where  conind='-1' and congas='on' ";
+			$sFuncion = "Cron Saldos - Inicio eliminación - ".date("Y-m-d")." ".date("H:i:s");
+			$sDescripcion = "Se inicia proceso de eliminación de registros en ".$empresa."_000014.";
+			insertarLogCron($sFuncion, $sDescripcion);
+			
+			$query = "delete  from ".$empresa."_000014 ";
+			$query .= "  where Salano = ".$wano;
+			$query .= "       and Salmes = ".$wmes;
+			$err = mysql_query($query,$conex) or die(mysql_errno().":".mysql_error());
 
-			$err2 = mysql_query($query,$conex);
-			$row2 = mysql_fetch_array($err2);
+			$sFuncion = "Cron Saldos - Fin eliminación - ".date("Y-m-d")." ".date("H:i:s");
+			$sDescripcion = "Se finaliza proceso de eliminación de registros en ".$empresa."_000014.";
+			insertarLogCron($sFuncion, $sDescripcion);
+
+			echo "<table border=0 align=center>";
+			echo "<tr><td class='texto4'><font face='tahoma'><b>AÑO DE PROCESO : </b>".$wano."</td></tr>";
+			echo "<tr><td class='texto4'><font face='tahoma'><b>MES DE PROCESO : </b>".$wmes."</td></tr></table><br><br>";
+			$ins=array();
+			//consultamos en la tabla de saldos de insumos, las cantidades
+			$query = "SELECT  Appcco, Apppre, Appexi, Appcnv, Appcos, Appcod, Appest  from ".$empresa."_000009 order by Apppre ";
+			$err = mysql_query($query,$conex);
+			$num = mysql_num_rows($err);
 
 			for ($i=0;$i<$num;$i++)
 			{
 				$row = mysql_fetch_array($err);
-				//consultamos los valores para productos codificados
-				//para esto hay que desglosarlo primero en insumos
-				$res=calcularProducto($row[2],$row[0].'-'.$row[1], $ins, $row2[0]);
+				$ins[$i]['cco']=$row[0];
+				$ins[$i]['cod']=$row[1];
+				$ins[$i]['exi']=$row[2];
+				$ins[$i]['cnv']=$row[3];
+				$ins[$i]['cos']=$row[4];
+				$ins[$i]['ins']=$row[5];
+				$ins[$i]['est']=$row[6];
 			}
-		}
 
-		echo "<b>Registros Totales Kardex : ".count($ins)."</b><br><br>";
-		if(count($ins)>0)
-		{
-			$contador=0;
-			for ($i=0;$i<count($ins);$i++)
+			//consultamos en la tabla de lotes, los productos con saldo
+			$query = "SELECT plocod, plopro, plosal from ".$empresa."_000004 where plosal>0 and ploest='on' ";
+			$err = mysql_query($query,$conex);
+			$num = mysql_num_rows($err);
+			if ($num>0)
 			{
-				$fecha = date("Y-m-d");
-				$hora = (string)date("H:i:s");
-				$query = "insert ".$empresa."_000014 (medico,fecha_data, hora_data, Salano, Salmes, Salcod,             Salcco,               Salexi,               Salpro,            Salvuc,               Salmax, Salmin, Salpor, Salfuc, Salins, Salest, seguridad) ";
-				$query .= "values ('".$empresa."','".$fecha."','".$hora."', ".$wano.",".$wmes.",'".$ins[$i]['cod']."','".$ins[$i]['cco']."','".$ins[$i]['exi']."','".$ins[$i]['cnv']."','".$ins[$i]['cos']."', 0,      0,    0,  '".date('Y-m-d')."', '".$ins[$i]['ins']."','".$ins[$i]['est']."', 'C-".$empresa."')";
-				$err3 = mysql_query($query,$conex);
-				if ($err3 != 1)
-				echo mysql_errno().":".mysql_error()."<br>";
-				else
+				//consultamos el movimiento de fabricación del lotes
+				$query = "SELECT concod from   ".$empresa."_000008 ";
+				$query .= " where  conind='-1' and congas='on' ";
+
+				$err2 = mysql_query($query,$conex);
+				$row2 = mysql_fetch_array($err2);
+
+				for ($i=0;$i<$num;$i++)
 				{
-					$contador++;
-					echo "REGISTRO INSERTADO NRo : ".$contador."<br>";
+					$row = mysql_fetch_array($err);
+					//consultamos los valores para productos codificados
+					//para esto hay que desglosarlo primero en insumos
+					$res=calcularProducto($row[2],$row[0].'-'.$row[1], $ins, $row2[0]);
 				}
 			}
-			echo "<b>TOTAL REGISTROS INSERTADOS  : </b>".$contador."<br>";
+
+			echo "<b>Registros Totales Kardex : ".count($ins)."</b><br><br>";
+			$sFuncion = "Cron Saldos - Registros Totales Kardex - ".date("Y-m-d")." ".date("H:i:s");
+			$sDescripcion = "Registros Totales Kardex a procesar : ".count($ins);
+			insertarLogCron($sFuncion, $sDescripcion);
+
+			if(count($ins)>0)
+			{
+				$contador=0;
+				for ($i=0;$i<count($ins);$i++)
+				{
+					$fecha = date("Y-m-d");
+					$hora = (string)date("H:i:s");
+					$query = "insert ".$empresa."_000014 (medico,fecha_data, hora_data, Salano, Salmes, Salcod,             Salcco,               Salexi,               Salpro,            Salvuc,               Salmax, Salmin, Salpor, Salfuc, Salins, Salest, seguridad) ";
+					$query .= "values ('".$empresa."','".$fecha."','".$hora."', ".$wano.",".$wmes.",'".$ins[$i]['cod']."','".$ins[$i]['cco']."','".$ins[$i]['exi']."','".$ins[$i]['cnv']."','".$ins[$i]['cos']."', 0,      0,    0,  '".date('Y-m-d')."', '".$ins[$i]['ins']."','".$ins[$i]['est']."', 'C-".$empresa."')";
+					$err3 = mysql_query($query,$conex);
+					if ($err3 != 1)
+					echo mysql_errno().":".mysql_error()."<br>";
+					else
+					{
+						$contador++;
+						echo "REGISTRO INSERTADO NRo : ".$contador."<br>";
+						$sFuncion = "Cron Saldos - Registro ".$contador." ".date("Y-m-d")." ".date("H:i:s");
+						$sDescripcion = "Registro nro : ".$contador." insertado exitosamente ".date("Y-m-d")." ".date("H:i:s");
+						insertarLogCron($sFuncion, $sDescripcion);
+					}
+				}
+				echo "<b>TOTAL REGISTROS INSERTADOS  : </b>".$contador."<br>";
+				
+				$sFuncion = "Cron Saldos - Fin exitoso ".date("Y-m-d")." ".date("H:i:s");
+				$sDescripcion = 'Cron de saldos ejecutado exitosamente. Total registros insertados: '.$contador;
+				insertarLogCron($sFuncion, $sDescripcion);
+			}
+			else
+			{
+				echo "<br><br><center><table border=0 aling=center>";
+				echo "<tr><td><IMG SRC='/matrix/images/medical/root/cabeza.gif' ></td><tr></table>";
+				echo "<font size=3><MARQUEE BEHAVIOR=SCROLL BGCOLOR=#33FFFF LOOP=-1>AÑO -- MES  : YA FUE GENERADO  !!!!</MARQUEE></FONT>";
+				echo "<input type='submit' value='Continuar'></center>";
+				echo "<br><br>";
+
+				$sFuncion = "Cron Saldos - Fin sin inserción".date("Y-m-d")." ".date("H:i:s");
+				$sDescripcion = "No se generó error, pero se detectó que Año {$wano} - Mes {$wmes} ya fue generado. No se almacenó ningún registro.";
+				insertarLogCron($sFuncion, $sDescripcion);
+			}
 		}
-		else
-		{
-			echo "<br><br><center><table border=0 aling=center>";
-			echo "<tr><td><IMG SRC='/matrix/images/medical/root/cabeza.gif' ></td><tr></table>";
-			echo "<font size=3><MARQUEE BEHAVIOR=SCROLL BGCOLOR=#33FFFF LOOP=-1>AÑO -- MES  : YA FUE GENERADO  !!!!</MARQUEE></FONT>";
-			echo "<input type='submit' value='Continuar'></center>";
-			echo "<br><br>";
-		}
-	}
+	} catch (Exception $e) {
+		echo "<b>Cron Saldos - Fin con error ".date("Y-m-d")." ".date("H:i:s")."</b><br> Error: ".$e->getMessage()."<br><br>";
+		$sFuncion = "Cron Saldos - Fin con error ".date("Y-m-d")." ".date("H:i:s");
+		$sDescripcion = 'El error fue: '.$e->getMessage();
+		insertarLogCron($sFuncion, $sDescripcion);
+    }
 
 ?>
 </body>
