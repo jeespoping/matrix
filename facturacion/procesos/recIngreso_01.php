12547cd19e48e6b6d25954fc9d88d2eeee57d895
Brigith Lagares <blagarescarpio@gmail.com>
2022-02-11 10:11:20 -0500

    Se realiza correciones de query multiempresa.

--- facturacion/procesos/recIngreso_01.php
+++ facturacion/procesos/recIngreso_01.php
@@ -31,7 +31,7 @@
 
         $conex = obtenerConexionBD("matrix");
         $conex_o = odbc_connect('facturacion','','')  or die("No se realizo conexión con la BD de Facturación");
-        $wactualiz = "Versión: 1.0 07-Febrero-2020";
+        $wactualiz = "2021-11-16";
     }
     session_start();
 
@@ -134,13 +134,18 @@
                     $empCod = odbc_result($commEmpresa, 1); $empNom = odbc_result($commEmpresa, 3);
                 }
 
+                $wemp_pmla = $_REQUEST['wemp_pmla'];
+                $wbasedato1 = consultarInstitucionPorCodigo($conex, $wemp_pmla);
+                $wnit = $wbasedato1->nit;
+                $wnombre = $wbasedato1->nombre;
+
                 ?>
                 <div id="divContenido" class="divContenido">
                     <div id="divEncabezado" class="divEncabezado" align="center">
                         <div>
-                            <span><label>PROMOTORA MEDICA LAS AMERICAS S.A.</label></span>
+                            <span><label><?php $wnombre; ?></label></span>
                             <br>
-                            <span><label>Nit. 800067065</label></span>
+                            <span><label>Nit. <?php echo $wnit; ?></label></span>
                         </div>
                         <div align="right" style="width: 145px; float: right; margin-top: -50px; margin-right: 55px">
                             <img src="http://mx.lasamericas.com.co/matrix/images/medical/paf/logo.png" width="190" height="120">
