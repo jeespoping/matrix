c3a2eb324b7932077810d716959aaa9edfbf7463
Daniel Corredor Bustamante <daniel.corredor@lasamericas.com.co>
2022-06-01 15:24:37 -0500

    se corrige parametro quemado

--- hce/procesos/ordenesidc.inc.php
+++ hce/procesos/ordenesidc.inc.php
@@ -99,7 +99,7 @@ $conex = obtenerConexionBD("matrix");
  **********************************/
 
 global $wemp_pmla;
-$esquemaBDHce =  consultarAliasPorAplicacion($conex,$wemp_pmla,"hceidc");
+$esquemaBDHce =  consultarAliasPorAplicacion($conex,$wemp_pmla,"hce");
 $horaCorteDispensacion 	= consultarAliasPorAplicacion($conex,$wemp_pmla,"horaCorteDispensacion");
 $inicioDiaDispensacion 	= consultarAliasPorAplicacion($conex,$wemp_pmla,"inicioDiaDispensacion");
 $topePorcentualCtc 		= consultarAliasPorAplicacion($conex,$wemp_pmla,"topePorcentualCTC");
